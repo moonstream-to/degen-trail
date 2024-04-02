c8105e5b7a43160106574826f09bd5cb31e98d3d
Neeraj Kashyap <nkashy1@gmail.com>
2024-04-02 05:43:42 -0700

    Game contract placeholder

---
+++ src/Game.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+/**
+ * @title Degen Trail game contract
+ * @author Moonstream Engineering (engineering@moonstream.to)
+ *
+ * This is the game contract for The Degen Trail, a fully on-chain degenerate homage to The Oregon
+ * Trail.
+ */
+contract DegenTrail {}
